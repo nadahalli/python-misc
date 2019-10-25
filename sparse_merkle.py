c1e7fab9d47132d45c9652b6d3431bfba90389f4
Tejaswi Nadahalli <nadahalli@gmail.com>
2019-10-25 15:08:40 +0200

    Recursion works

--- sparse_merkle.py
+++ sparse_merkle.py
@@ -6,28 +6,10 @@ class SparseMerkle:
         return int(2 ** math.ceil(math.log2(sub_tree_root + 1)))
 
     def get_height(self, sub_tree_root):
-        return math.log2(self.next_power_of_2(sub_tree_root))
+        return int(math.log2(self.next_power_of_2(sub_tree_root)))
 
-    def find_common_ancestor(self, x, y):
-        if x < 1 or y < 1:
-            raise ValueError("Paramters must be greater than Zero")
-        if x == y:
-            return x
-        lower = min(x, y)
-        lower_height = self.get_height(lower)
-        
-        higher = max(x, y)
-        higher_height = self.get_height(higher)
-        
-        while (lower_height != higher_height):
-            higher = higher/2
-            higher_height = self.get_height(higher)
-            
-        while(lower != higher):
-            lower = int(lower/2)
-            higher = int(higher/2)
-                
-        return lower
+    def is_leaf(self, sub_tree_root, global_height):
+        return 2 ** (global_height - 1) <= sub_tree_root < 2 ** (global_height)
 
     def get_hash(self, x):
         return hashlib.sha256(x.encode('utf-8')).hexdigest()[54:]
@@ -36,34 +18,43 @@ class SparseMerkle:
         return self.get_hash(x + '-' + y)
 
     def get_null_proofs(self, size):
-        null_proofs = {0: self.get_hash('0')}
-        for i in range(size):
+        null_proofs = {1: self.get_hash('0')}
+        for i in range(1, size):
             null_proofs[i + 1] = self.get_concat(null_proofs[i],
                                                  null_proofs[i])
         return null_proofs
 
-    def attach_null_proofs_till(self, index, sub_tree_root, null_proofs):
-        null_proof_index = 0
-        non_null_proof = get_hash('1')
-        while (index != sub_tree_root):
-            if (index % 2 == 0):
-                non_null_proof = self.get_concat(non_null_proof,
-                                                 null_proofs[null_proof_index])
-            else:
-                non_null_proof = self.get_concat(null_proofs[null_proof_index],
-                                                 non_null_proof)
-            null_proof_index += 1
-            index = int(index/2)
-        return non_null_proof
+    def present(self, sub_tree_root, global_height, tree):
+        min_range = max_range = sub_tree_root
+        for i in range(self.get_height(sub_tree_root), global_height):
+            min_range = min_range * 2
+            max_range = max_range * 2 + 1
 
-
-    def find_root(self, tree, null_proofs):
-        keys = sorted(tree.keys())
+        offset = 2 ** (global_height - 1)
+        for x in sorted(tree):
+            if min_range <= x + offset <= max_range:
+                return True
+        return False
         
-        prev_subtree_root = 1
-        for key in keys:
-            pass
 
+    def find_root(self, sub_tree_root, global_height, tree, null_proofs):
+        if(self.present(sub_tree_root, global_height, tree)):
+            if (self.is_leaf(sub_tree_root, global_height)):
+                return self.get_hash(sub_tree_root)
+            else:
+                left_proof = self.find_root(sub_tree_root * 2,
+                                            global_height,
+                                            tree,
+                                            null_proofs)
+                right_proof = self.find_root(sub_tree_root * 2 + 1,
+                                             global_height,
+                                             tree,
+                                             null_proofs)
+                return self.get_concat(left_proof, right_proof)
+        else:
+            return null_proofs[
+                global_height - self.get_height(sub_tree_root) + 1]
+        
 if __name__ == '__main__':
     print(get_null_proofs(10))
     
--- sparse_merkle_test.py
+++ sparse_merkle_test.py
@@ -8,7 +8,7 @@ class TestSparseMerkle(unittest.TestCase):
         self.x = SparseMerkle()
 
         def side_effect(value):
-            return value
+            return str(value)
         
         self.x.get_hash = MagicMock(side_effect = side_effect)
         
@@ -17,28 +17,35 @@ class TestSparseMerkle(unittest.TestCase):
         self.assertEqual(self.x.get_height(3), 2)
         self.assertEqual(self.x.get_height(9), 4)
     
-    def test_find_common_ancestor(self):
-        self.assertEqual(self.x.find_common_ancestor(2, 3), 1)
-        self.assertEqual(self.x.find_common_ancestor(4, 6), 1)
-        self.assertEqual(self.x.find_common_ancestor(5, 6), 1)
-        self.assertEqual(self.x.find_common_ancestor(4, 5), 2)
-        self.assertEqual(self.x.find_common_ancestor(4, 11), 2)
-        self.assertEqual(self.x.find_common_ancestor(4, 14), 1)
-        self.assertEqual(self.x.find_common_ancestor(4, 4), 4)
-        self.assertRaises(ValueError, lambda: self.x.find_common_ancestor(0, 1))
-
     def test_get_concat(self):
         self.assertEqual(self.x.get_concat('1', '2'), '1-2')
 
     def test_get_null_proofs(self):
         expected = {}
-        expected[0] = '0'
-        expected[1] = '-'.join(['0' for i in range(2)])
-        expected[2] = '-'.join(['0' for i in range(4)])
-        expected[3] = '-'.join(['0' for i in range(8)])
-        expected[4] = '-'.join(['0' for i in range(16)])
-        expected[5] = '-'.join(['0' for i in range(32)])
-        self.assertEqual(self.x.get_null_proofs(5), expected)
+        expected[1] = '0'
+        expected[2] = '-'.join(['0' for i in range(2)])
+        expected[3] = '-'.join(['0' for i in range(4)])
+        expected[4] = '-'.join(['0' for i in range(8)])
+        expected[5] = '-'.join(['0' for i in range(16)])
+        expected[6] = '-'.join(['0' for i in range(32)])
+        self.assertEqual(self.x.get_null_proofs(6), expected)
+
+    def test_present(self):
+        self.assertEqual(self.x.present(2, 4, [1]), True)
+        self.assertEqual(self.x.present(1, 4, [7]), True)
+        self.assertEqual(self.x.present(2, 4, [4, 5]), False)
+        self.assertEqual(self.x.present(2, 4, [3, 5]), True)
+
+    def test_find_root(self):
+        null_proofs = self.x.get_null_proofs(4)
+        self.assertEqual(self.x.find_root(1, 4, [], null_proofs),
+                         '0-0-0-0-0-0-0-0')
+        self.assertEqual(self.x.find_root(1, 4, [0], null_proofs),
+                         '8-0-0-0-0-0-0-0')
+        self.assertEqual(self.x.find_root(1, 4, [0, 4], null_proofs),
+                         '8-0-0-0-12-0-0-0')
+        self.assertEqual(self.x.find_root(1, 4, [0, 1, 2, 3, 4, 5, 6, 7], null_proofs),
+                         '8-9-10-11-12-13-14-15')
 
 
 if __name__ == '__main__':
