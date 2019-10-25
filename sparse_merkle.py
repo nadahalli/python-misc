2227bc1d79f3824f2d2ecabdf3290f4baf29d4c3
Tejaswi Nadahalli <nadahalli@gmail.com>
2019-10-26 00:36:34 +0200

    Moved some methods inside

--- sparse_merkle.py
+++ sparse_merkle.py
@@ -2,20 +2,55 @@ import hashlib
 import math
 
 class SparseMerkle:
+    def __init__(self, global_height, get_hash = None):
+        self.global_height = global_height
+        if get_hash:
+            self.get_hash = get_hash
+        self.null_proofs = self.get_null_proofs(global_height)
+        self.tree = {}
+
+
+    def add_elem(self, x):
+        self.tree[x] = []
+
+
+    def add_elems(self, many):
+        for x in many:
+            self.add_elem(x)
+
+
+    def remove_elem(self, x):
+        del self.tree[x]
+        
+
+    def remove_all(self):
+        for key in list(self.tree.keys()):
+            del self.tree[key]
+            
+        
+    def get_tree(self):
+        return self.tree
+
+    
     def next_power_of_2(self, sub_tree_root):
         return int(2 ** math.ceil(math.log2(sub_tree_root + 1)))
+    
 
     def get_height(self, sub_tree_root):
         return int(math.log2(self.next_power_of_2(sub_tree_root)))
+    
 
-    def is_leaf(self, sub_tree_root, global_height):
-        return 2 ** (global_height - 1) <= sub_tree_root < 2 ** (global_height)
+    def is_leaf(self, sub_tree_root):
+        return 2 ** (self.global_height - 1) <= sub_tree_root < 2 ** (self.global_height)
+    
 
     def get_hash(self, x):
         return hashlib.sha256(x.encode('utf-8')).hexdigest()[54:]
+    
 
     def get_concat(self, x, y):
         return self.get_hash(x + '-' + y)
+    
 
     def get_null_proofs(self, size):
         null_proofs = {1: self.get_hash('0')}
@@ -23,56 +58,62 @@ class SparseMerkle:
             null_proofs[i + 1] = self.get_concat(null_proofs[i],
                                                  null_proofs[i])
         return null_proofs
+    
 
-    def present(self, sub_tree_root, global_height, tree):
+    def present(self, sub_tree_root):
         min_range = max_range = sub_tree_root
-        for i in range(self.get_height(sub_tree_root), global_height):
+        for i in range(self.get_height(sub_tree_root), self.global_height):
             min_range = min_range * 2
             max_range = max_range * 2 + 1
 
         present_list = []
-        offset = 2 ** (global_height - 1)
-        for x in sorted(tree.keys()):
+        offset = 2 ** (self.global_height - 1)
+        for x in sorted(self.tree.keys()):
             if min_range <= x + offset <= max_range:
                 present_list.append(x)
         return present_list
         
 
-    def find_root(self, sub_tree_root, global_height, tree, null_proofs):
-        present_list = self.present(sub_tree_root, global_height, tree)
+    def find_root_internal(self, sub_tree_root):
+        present_list = self.present(sub_tree_root)
         if (len(present_list) > 0):
-            if (self.is_leaf(sub_tree_root, global_height)):
-                proof = self.get_hash(sub_tree_root)
-                tree[present_list[0]].append(proof)
+            if (self.is_leaf(sub_tree_root)):
+                root = self.get_hash(sub_tree_root)
+                self.tree[present_list[0]].append(root)
                 if (sub_tree_root % 2 == 0):
-                    tree[present_list[0]].insert(0, '(')
+                    self.tree[present_list[0]].insert(0, '(')
                 else:
-                    tree[present_list[0]].append(')')
-                return proof, tree, present_list
+                    self.tree[present_list[0]].append(')')
+                return root, present_list
             else:
-                left_root, tree, left_list = self.find_root(sub_tree_root * 2,
-                                                            global_height,
-                                                            tree,
-                                                            null_proofs)
-                right_root, tree, right_list = self.find_root(sub_tree_root * 2 + 1,
-                                                              global_height,
-                                                              tree,
-                                                              null_proofs)
-                proof = self.get_concat(left_root, right_root)
+                left_root, left_list = self.find_root_internal(sub_tree_root * 2)
+                right_root, right_list = self.find_root_internal(sub_tree_root * 2 + 1)
+
+                root = self.get_concat(left_root, right_root)
                 for present_item in present_list:
                     if present_item in left_list:
-                        tree[present_item].append(right_root)
-                        tree[present_item].append(')')
+                        self.tree[present_item].append(right_root)
+                        self.tree[present_item].append(')')
                     elif present_item in right_list:
-                        tree[present_item].insert(0, left_root)
-                        tree[present_item].insert(0, '(')
+                        self.tree[present_item].insert(0, left_root)
+                        self.tree[present_item].insert(0, '(')
                     else:
                         assert(False)
-                return proof, tree, present_list
+                return root, present_list
         else:
-            proof = null_proofs[
-                global_height - self.get_height(sub_tree_root) + 1]
-            return proof, tree, present_list
+            return self.null_proofs[
+                self.global_height - self.get_height(sub_tree_root) + 1], present_list
+
+
+    def find_root(self):
+        for key in self.tree.keys():
+            self.tree[key].clear()
+
+        return self.find_root_internal(1)[0]
+
+
+    def get_inclusion_proof(self, ):
+        pass
 
 
 if __name__ == '__main__':
--- sparse_merkle_test.py
+++ sparse_merkle_test.py
@@ -1,16 +1,12 @@
 from sparse_merkle import SparseMerkle
-from unittest.mock import MagicMock
 
 import unittest
 
 class TestSparseMerkle(unittest.TestCase):
     def setUp(self):
-        self.x = SparseMerkle()
-
-        def side_effect(value):
+        def get_hash(value):
             return str(value)
-        
-        self.x.get_hash = MagicMock(side_effect = side_effect)
+        self.x = SparseMerkle(4, get_hash)
         
     def test_get_height(self):
         self.assertEqual(self.x.get_height(2), 2)
@@ -31,22 +27,34 @@ class TestSparseMerkle(unittest.TestCase):
         self.assertEqual(self.x.get_null_proofs(6), expected)
 
     def test_present(self):
-        self.assertEqual(self.x.present(2, 4, {1:[]}), [1])
-        self.assertEqual(self.x.present(1, 4, {7:[]}), [7])
-        self.assertEqual(self.x.present(2, 4, {4:[], 5:[]}), [])
-        self.assertEqual(self.x.present(2, 4, {3:[], 5:[]}), [3])
-        self.assertEqual(self.x.present(1, 4, {3:[], 5:[]}), [3, 5])
+        self.x.remove_all()
+        self.x.add_elems([1])
+        self.assertEqual(self.x.present(2), [1])
+
+        self.x.remove_all()
+        self.x.add_elems([7])
+        self.assertEqual(self.x.present(1), [7])
+
+        self.x.remove_all()
+        self.x.add_elems([4, 5])
+        self.assertEqual(self.x.present(2), [])
+
+        self.x.remove_all()
+        self.x.add_elems([3, 5])
+        self.assertEqual(self.x.present(2), [3])
+        self.assertEqual(self.x.present(1), [3, 5])
 
     def test_find_root(self):
-        null_proofs = self.x.get_null_proofs(4)
-        self.assertEqual(self.x.find_root(1, 4, {0: [], 1: [], 3: [], 6: [], 7: []}, null_proofs),
-                         ('8-9-0-11-0-0-14-15',
+        self.x.remove_all()
+        self.x.add_elems([0, 1, 3, 6, 7])
+        self.assertEqual(self.x.find_root(), '8-9-0-11-0-0-14-15')
+        self.assertEqual(self.x.get_tree(),
                           {0: ['(', '8', '9', ')', '0-11', ')', '0-0-14-15', ')'],
                            1: ['(', '8', '9', ')', '0-11', ')', '0-0-14-15', ')'],
                            3: ['(', '8-9', '(', '0', '11', ')', '0-0-14-15', ')'],
                            6: ['(', '8-9-0-11', '(', '0-0', '(', '14', '15', ')'],
-                           7: ['(', '8-9-0-11', '(', '0-0', '(', '14', '15', ')']},
-                          [0, 1, 3, 6, 7]))
+                           7: ['(', '8-9-0-11', '(', '0-0', '(', '14', '15', ')']})
+
 
 if __name__ == '__main__':
     unittest.main()
