ab2d308d398588cdea0a6c527a99a1ffb305680b
Tejaswi Nadahalli <nadahalli@gmail.com>
2020-01-21 15:02:16 +0100

    Comment

--- search.py
+++ search.py
@@ -9,11 +9,13 @@ def search(a, origin, point):
                 return search(a, origin, 0)
         point = point * 2 + 1
 
+    # Figure out a way to write this if condition as a part of the main else in the while loop
     if (origin + point > end):
         origin = origin + int((point - 1) / 2) + 1
         return search(a, origin, 0)
 
     return -1
+
 
 def create(m, n):
     a = [0 for i in range(m)]
@@ -21,9 +23,10 @@ def create(m, n):
     a.extend(b)
     return a
 
+
 if __name__ == '__main__':
-    assert(search(create(10, 20), 0, 0) == 10)
-    assert(search(create(16, 32), 0, 0) == 16)
-    assert(search(create(0, 20), 0, 0) == 0) # all 1s
-    assert(search(create(20, 0), 0, 0) == -1) # all 0s
-    assert(search(create(0, 0), 0, 0) == -1) # no elements
+    assert (search(create(10, 20), 0, 0) == 10)
+    assert (search(create(16, 32), 0, 0) == 16)
+    assert (search(create(0, 20), 0, 0) == 0)  # all 1s
+    assert (search(create(20, 0), 0, 0) == -1)  # all 0s
+    assert (search(create(0, 0), 0, 0) == -1)  # no elements
