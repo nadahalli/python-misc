d6e78965bd7de8e08f685fe1aec55b5bf5be42a1
Tejaswi Nadahalli <nadahalli@gmail.com>
2020-10-17 00:49:32 +0200

    Generate graph isomorphisms

---
+++ isomorphism.py
@@ -0,0 +1,35 @@
+from networkx import random_regular_graph
+from random import sample
+
+def generate_isomorphism(g):
+    vertices = set([])
+    for e in g.edges():
+        vertices.add(e[0])
+        vertices.add(e[1])
+
+    mapped_vertices = {}
+    edges = []
+    for e in g.edges():
+        new_edge = []
+        for i in [0, 1]:
+            if e[i] not in mapped_vertices:
+                replacement = sample(vertices, 1)[0]
+                vertices.remove(replacement)
+                mapped_vertices[e[i]] = replacement
+            else:
+                replacement = mapped_vertices[e[i]]
+                
+            new_edge.append(replacement)
+        edges.append(tuple(new_edge))
+    return edges
+
+k = 20
+
+g = random_regular_graph(k, 2*k+1)
+
+#print(",".join(map(str, g.edges())))
+for i in range(100000):
+    print(1, ",".join(map(str, generate_isomorphism(g))))
+
+for i in range(1000):
+    print(0, random_regular_graph(k, 2*k+1))
